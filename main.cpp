dbb23b6f11464f0fe7016c725cdb4b15fbf9cefe
Youssef Okasha <36171027+yousefokasha61@users.noreply.github.com>
2026-09-18 15:02:52 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TODO (frequency): implement per the lesson description.
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        if (line.empty()) continue;
+        cout << "TODO" << "\n";
+    }
+}
