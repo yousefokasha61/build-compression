d321012641a63e45cb4a9c07e17e80712986683a
Youssef Okasha <36171027+yousefokasha61@users.noreply.github.com>
2026-09-18 17:54:35 +0300

    first practice

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 // TODO (frequency): implement per the lesson description.
 
 int main() {
     string line;
-    while (getline(cin, line)) {
-        if (line.empty()) continue;
-        cout << "TODO" << "\n";
+    map<char, int> freq;
+    if (getline(cin, line)) {
+        if (line.empty()) return 0;
+        for (char c : line) {
+            if (isalpha(c)) {
+                freq[tolower(c)]++;
+            }
+        }
+        for (map<char, int>::iterator it = freq.begin(); it != freq.end(); ++it) {
+            cout << it->first << " " << it->second << endl;
+        }
+        freq.clear();
     }
+    return 0;
 }
