0f263b27f1ca9d5b7d3550c451b22ac52421d40b
Youssef Okasha <36171027+yousefokasha61@users.noreply.github.com>
2026-09-19 17:29:08 +0300

    fix second practice

--- main.cpp
+++ main.cpp
@@ -13,9 +13,7 @@ int main() {
     if (getline(cin, line)) {
         if (line.empty()) return 0;
         for (char c : line) {
-            if (isalpha(c)) {
-                freq[c]++;
-            }
+            freq[c]++;
         }
         double entropy = 0.0;
         for (map<char, int>::iterator it = freq.begin(); it != freq.end(); ++it) {
@@ -23,10 +21,11 @@ int main() {
             double prob = it->second / (double)line.length();
             entropy += -prob * log2(prob);
         }
+        double min_size = entropy * line.length() / 8.0;
         // print with 4 decimal places
         cout << "bytes=" << line.length() << endl;
         std::cout << "entropy=" << std::fixed << std::setprecision(4) << entropy << std::endl;
-        std::cout << "min_size=" << std::fixed << std::setprecision(3) << entropy << std::endl;
+        std::cout << "min_size=" << std::fixed << std::setprecision(3) << min_size << std::endl;
     }
     // calculate entropy
     return 0;
